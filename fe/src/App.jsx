ad881d9bccdfa66b15a814eb7e7384662f1cf19b
huetv2303 <tuonghue2303@gmail.com>
2026-03-12 10:57:11 +0700

    first commit

---
+++ fe/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + Tailwind v3 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
